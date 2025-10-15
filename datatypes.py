63ef1b8bbca1782379eae85be5c3151c2ed450f7
Naeem847 <ashoornaeem@gmail.com>
2025-10-15 11:01:03 +0500

    Create datatypes.py
    python data types and variables and second task control structure in for loop and others.

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+# task 2 Variables and data types:
+name="Ai student"
+age=22
+marks=[85,90,78]
+student={"name":name,"age":age,"marks":marks}
+print(student)
+# task 3 control structure:
+for i in range(1,6):
+    if i%2==0:
+        print(i,"is even")
+    else:
+        print(i,"is odd")
+#task 4 functions:
+
+
+        
