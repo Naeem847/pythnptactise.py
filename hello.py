d2fba57d2a1f0e36900deb9c55c934e36e150d01
Naeem847 <ashoornaeem@gmail.com>
2025-12-10 19:57:23 +0500

    Update hello.py
    This program includes four simple Python tasks made for students who are beginning to learn Artificial Intelligence and Python. Each task shows a basic idea of how Python works.

--- hello.py
+++ hello.py
@@ -1,58 +1,33 @@
-# print("wellcome to artificial intelligence Lab:")
-# name="Ai student"
-# age=22
-# marks=[85,90,78]
-# student={"name":name,"age":age,"marks":marks}
-# print(student)
-# for i in range(1,6):
-#     if i%2==0:
-#         print(i,"is even")
-#     else:
-#         print("odd")    
-# def factorial(n):
-#     if n==0 or n==1:
-#         return 1
-#     else:
-#         return n*factorial(n-1)
-# print("factorial of 5:",factorial(5))
-# class student:
-#     def __init__(self, name, rollno):
-#         self.name = name
-#         self.rollno = rollno
-#     def display(self):
-#         print("name:", self.name)
-#         print("rollno:", self.rollno)
-# s1 = student("ali", 101)
-# s1.display()
-print("Ai chatbot: Hello!BSCS6C i am your virtual assistant.")
-print("type'bye' to end the chat.\n")
-while True:
-    user_input=input("you:").lower()
-    if"hello"in user_input or "hi" in user_input:
-         print("chatbot:hello! how can i help you today?")
-    elif "how are you"in user_input:
-         print("chatbot:i am just a computer program,but i am here to assist you!")
-    elif"what is your name" in user_input:
-         print("chatbot:i don't have a name.i'm just a program.")
-    elif"what can you do"in user_input:
-         print("chatbot:i can answer question provide information, and  conversation")
-    elif"how old are you"in user_input:
-         print("Chatbot:i don't age.i am a program designed to assist you.")
-    elif"tell a joke"in user_input: 
-         print("Chatbot:why don't scientists trust atoms?because they make up everything!")
-    elif"what is the capital of pakistan"in user_input: 
-         print("Chatbot:the capital of pakistan is islamabad")  
-    elif"your name"in user_input: 
-         print("Chatbot:i'm an AI chatbot created for this job.")
-    elif"ai"in user_input:
-         print("Chatbot:AI stand for Artificial Intelligence-simulating human thinking.")
-    elif"creator"in user_input:
-         print("Chatbot:i was created by Muhammad Naeem for BSCS6C student of FUUAST")
-    elif"bye"in user_input:
-         print("Chatbot:Goodbye!Hava a great day!")
-         break
+#Task 1:
+print("wellcome to artificial intelligence Lab:")
+name="Ai student"
+age=22
+marks=[85,90,78]
+student={"name":name,"age":age,"marks":marks}
+print(student)
+#Task 2:
+for i in range(1,6):
+    if i%2==0:
+        print(i,"is even")
     else:
-         print("Chatbot:Sory i didn't understand that.")
+        print("odd") 
+#Task 3:   
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+print("factorial of 5:",factorial(5))
+#Task 4:
+class student:
+    def __init__(self, name, rollno):
+        self.name = name
+        self.rollno = rollno
+    def display(self):
+        print("name:", self.name)
+        print("rollno:", self.rollno)
+s1 = student("ali", 101)
+s1.display()
 
 
          
