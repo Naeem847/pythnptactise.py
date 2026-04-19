485ad4e6323f1742184a541b58f46309063c7007
Naeem847 <ashoornaeem@gmail.com>
2026-04-20 00:10:54 +0500

    create todo list app display the menu 1. is view task 2, is add task 3. is delete task 4. is exit the task tasks=[] is a empty list in which we are store the data and then 1. add the data 2. delete the data 3. is view the data 4 is exit the data also using the while loop for while loop to keep the program running continuously  so the user can choice the option again and again....

---
+++ todoo_list.py
@@ -0,0 +1,49 @@
+def display_menu():
+    print("To_do list display menu:")
+    print("1.view_task")
+    print("2.Add_task")
+    print("3.Delete_task")
+    print("4.Exit")
+
+def view_task(tasks):
+        if not tasks:
+            print("No tasks available!!!")
+        else:
+            print("your tasks:")
+            for i, task in enumerate(tasks, 1):
+                print(f"{i}. {task}")
+def add_task(tasks):
+    task=input("enter your task you want  to add!!!")
+    tasks.append(task)
+    print("task added successfully!!!")
+def delete_task(tasks):
+    view_task(tasks)
+    try:
+           task_num=int(input("enter the task number you want to delete!!!"))
+           if 1<=task_num<=len(tasks):
+                removed_task=tasks.pop(task_num-1)
+                print(f"task '{removed_task}' deleted successfully!!!")
+           else:
+               print("invalid task number!!!")
+    except ValueError:
+            print("please enter a valid number!!!")    
+               
+def to_do_list_app():
+    tasks=[]
+    while True:
+        display_menu()
+        choice=input("Enter your choice: ")
+        if choice=="1":
+            view_task(tasks)
+        elif choice=="2":
+            add_task(tasks)
+        elif choice=="3":
+            delete_task(tasks) 
+        elif choice=="4":
+            print("exiting the app good bye!!!")
+            break
+        else:
+            print("invalid chice please try again!!!")
+to_do_list_app()
+        
+    
