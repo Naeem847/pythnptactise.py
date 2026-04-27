8c7407d7a1769918805eb1600002ee63c128fd83
Naeem847 <ashoornaeem@gmail.com>
2026-04-27 21:40:30 +0500

    This repository contains a simple Python program that demonstrates basic function usage. It includes two functions: one for addition and one for subtraction. The program showcases how to define functions, pass parameters, and return results in Python.

---
+++ methods.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+print(add(5, 3))
+print(subtract(10, 4))
+
+# example for code with functions and loops
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
