5df93fbcb4d4cfe8fbb15b34e7a104ec3ea5d449
Naeem847 <ashoornaeem@gmail.com>
2026-05-03 23:48:52 +0500

    A simple Python example showing runtime polymorphism, where different objects respond differently to the same method (sound()).

---
+++ oop_concepts.py
@@ -0,0 +1,15 @@
+class Animal:
+    def sound(self):
+        print("Animal makes sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+
+# Polymorphism in action
+for obj in [Dog(), Cat()]:
+    obj.sound()
