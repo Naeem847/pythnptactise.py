a17c7fb8155f81a3b860ae1f729ca5b0874b6fec
Naeem847 <ashoornaeem@gmail.com>
2026-05-06 15:26:38 +0500

    practice oop concepts polomorphism using for loop and list

--- oop_concepts.py
+++ oop_concepts.py
@@ -13,3 +13,21 @@ class Cat(Animal):
 # Polymorphism in action
 for obj in [Dog(), Cat()]:
     obj.sound()
+#   2nd example of polimorphism
+class Animal:
+    def sound(self):
+        print("Animal make sounds")
+
+class Dog(Animal):
+    def sound(self):
+        print("dog barks")
+
+class cat(Animal):
+    def sound(self):
+        print("cat meaws")
+
+animal=[Dog(),cat()]
+
+for a in animal:
+    a.sound()
+
